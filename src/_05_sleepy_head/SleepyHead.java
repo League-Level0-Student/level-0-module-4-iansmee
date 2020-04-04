465a7a48c35483a950c8f337010e0825eeb2c02e
iansmee <53161827+iansmee@users.noreply.github.com>
2020-04-04 11:27:55 -0700

    finished sleepy head and worked on string methods

--- src/_05_sleepy_head/SleepyHead.java
+++ src/_05_sleepy_head/SleepyHead.java
@@ -15,12 +15,33 @@ public class SleepyHead {
         /* You MUST use the above boolean variables in your code */
         
         /*
-         * Ask the user for these values using a confirm dialog like the one below
-         * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-         */
-    
+         * Ask the user for these values using a confirm dialog like the one below*/
+      int weekday  =  JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+         if(weekday == 0) {
+        	 isWeekday = true;
+         }
+         else {
+        	 isWeekday = false;
+         }
+         int vacation  =  JOptionPane.showConfirmDialog(null, "Are you on vacation", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+if(vacation == 0) {
+	isVacation = true;
+}
+else {
+	isVacation = false;
+}
 
 
+
+if(isVacation == true || isWeekday == false) {
+	System.out.println("sleep in");
+}
+if(isWeekday == true && isVacation == false) {
+	System.out.println("get up lazybones");
+}
+if(isWeekday == true && isVacation == true) {
+	System.out.println("sleep in");
+}
         /*
          * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!�? If it is a weekday, and we are on vacation,
